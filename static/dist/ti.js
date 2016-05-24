b6a136c81cc5053c96bfce8d46c2c3b5ef603e5d
Jason Priem <jp@jasonpriem.org>
2016-05-23 20:57:54 -0700

    sample page shows up.

--- static/dist/ti.js
+++ static/dist/ti.js
@@ -38,6 +38,13 @@ angular.module('aboutPages', [])
     })
 
     .config(function($routeProvider) {
+        $routeProvider.when('/sample', {
+            templateUrl: 'about-pages/sample.tpl.html',
+            controller: 'samplePageCtrl'
+        })
+    })
+
+    .config(function($routeProvider) {
         $routeProvider.when('/about/achievements', {
             templateUrl: 'about-pages/about-badges.tpl.html',
             controller: 'aboutPageCtrl',
@@ -71,6 +78,10 @@ angular.module('aboutPages', [])
             .success(function(resp){
                 $scope.numProfiles = resp.count
             })
+    })
+
+    .controller("samplePageCtrl", function(){
+        console.log("sample page ctrl!")
     })
 
 
@@ -1960,7 +1971,7 @@ angular.module('staticPages', [
 
 
 
-angular.module('templates.app', ['about-pages/about-badges.tpl.html', 'about-pages/about-data.tpl.html', 'about-pages/about-legal.tpl.html', 'about-pages/about-orcid.tpl.html', 'about-pages/about.tpl.html', 'about-pages/search.tpl.html', 'badge-page/badge-page.tpl.html', 'footer/footer.tpl.html', 'header/header.tpl.html', 'header/search-result.tpl.html', 'helps.tpl.html', 'loading.tpl.html', 'person-page/person-page-text.tpl.html', 'person-page/person-page.tpl.html', 'product-page/product-page.tpl.html', 'settings-page/settings-page.tpl.html', 'sidemenu.tpl.html', 'static-pages/landing.tpl.html', 'static-pages/login.tpl.html', 'static-pages/twitter-login.tpl.html', 'workspace.tpl.html']);
+angular.module('templates.app', ['about-pages/about-badges.tpl.html', 'about-pages/about-data.tpl.html', 'about-pages/about-legal.tpl.html', 'about-pages/about-orcid.tpl.html', 'about-pages/about.tpl.html', 'about-pages/sample.tpl.html', 'about-pages/search.tpl.html', 'badge-page/badge-page.tpl.html', 'footer/footer.tpl.html', 'header/header.tpl.html', 'header/search-result.tpl.html', 'helps.tpl.html', 'loading.tpl.html', 'person-page/person-page-text.tpl.html', 'person-page/person-page.tpl.html', 'product-page/product-page.tpl.html', 'settings-page/settings-page.tpl.html', 'sidemenu.tpl.html', 'static-pages/landing.tpl.html', 'static-pages/login.tpl.html', 'static-pages/twitter-login.tpl.html', 'workspace.tpl.html']);
 
 angular.module("about-pages/about-badges.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("about-pages/about-badges.tpl.html",
@@ -2226,6 +2237,22 @@ angular.module("about-pages/about.tpl.html", []).run(["$templateCache", function
     "\n" +
     "\n" +
     "   </div><!-- end wrapper -->\n" +
+    "</div>");
+}]);
+
+angular.module("about-pages/sample.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("about-pages/sample.tpl.html",
+    "<div class=\"page sample\">\n" +
+    "    <h2>Here's a sample of articles</h2>\n" +
+    "\n" +
+    "    <div class=\"main\">\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "    </div>\n" +
     "</div>");
 }]);
 
