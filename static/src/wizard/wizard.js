01ac37387a677065e69afd74f2bc405bc350913b
Jason Priem <jp@jasonpriem.org>
2016-07-29 18:18:43 -0700

    save point i guess.

--- static/src/wizard/wizard.js
+++ static/src/wizard/wizard.js
@@ -16,24 +16,6 @@ angular.module('wizard', [
         })
     })
 
-    .config(function ($routeProvider) {
-        $routeProvider.when('/orcid-connect', {
-            templateUrl: "wizard/orcid-connect.tpl.html",
-            controller: "OrcidConnectCtrl",
-            resolve: {
-                isLoggedIn: function($rootScope){
-                    return $rootScope.isAuthenticatedPromise()
-                }
-            }
-        })
-    })
-
-    .config(function ($routeProvider) {
-        $routeProvider.when('/twitter-register', {
-            templateUrl: "wizard/twitter-register.tpl.html",
-            controller: "TwitterRegisterCtrl"
-        })
-    })
 
     .config(function ($routeProvider) {
         $routeProvider.when('/wizard/my-publications', {
@@ -87,104 +69,6 @@ angular.module('wizard', [
 
     })
 
-
-
-    .controller("OrcidConnectCtrl", function($scope, $location, $http, $auth, $rootScope){
-        console.log("OrcidConnectCtrl running")
-        var searchObject = $location.search();
-        var code = searchObject.code
-
-        if (!code){
-            console.log("there is no oauth code in the url. quitting.")
-            $location.path("/")
-            return false
-        }
-
-        var requestObj = {
-            redirectUri: $rootScope.orcidRedirectUri.connect
-        }
-
-        console.log("POSTing the request code to the server", requestObj)
-        $http.post("api/me/orcid/oauth_code/" + code, requestObj)
-            .success(function(resp){
-                console.log("we successfully added an ORCID!", resp)
-                $auth.setToken(resp.token)
-                if ($auth.getPayload().num_products > 0) {
-                    console.log("they have some works, good! redirect to your-publications")
-                    $location.url("wizard/my-publications")
-                }
-                else {
-                    console.log("they have no works. redirect to page to add-publications")
-                    $location.url("wizard/add-publications")
-
-                }
-
-                //$rootScope.sendCurrentUserToIntercom()
-                //$location.url("u/" + payload.sub)
-            })
-            .error(function(resp){
-              console.log("problem getting token back from server!", resp)
-                //$location.url("/")
-            })
-
-
-
-    })
-
-
-
-    .controller("TwitterRegisterCtrl", function($scope, $location, $http, $auth, $rootScope){
-        console.log("TwitterRegisterCtrl running")
-
-
-        var searchObject = $location.search();
-        var token = searchObject.oauth_token
-        var verifier = searchObject.oauth_verifier
-
-        if (!token || !verifier){
-            console.log("twitter didn't give oauth_verifier and a oauth_token")
-            $location.url("/")
-            return false
-        }
-
-        var requestObj = {
-            token: token,
-            verifier: verifier
-        }
-
-        $http.post("api/auth/register/twitter", requestObj)
-            .success(function(resp){
-                $auth.setToken(resp.token)
-                if (resp.is_new_profile){
-                    console.log("registered a new user with twitter", resp)
-                    $location.url("wizard/welcome")
-                    //$rootScope.sendCurrentUserToIntercom()
-                }
-                else {
-                    console.log("an existing impactstory user logged in with twitter, using the register button", resp)
-                    if ($auth.getPayload().orcid_id){
-                        console.log("they've got a orcid in their profile. sending them to profile page.")
-                        $location.url("u/" + $auth.getPayload().orcid_id)
-                    }
-                    else {
-                        console.log("they are logged in with twitter, but no orcid yet.")
-                        $location.url("wizard/welcome")
-                    }
-                }
-
-
-
-            })
-            .error(function(resp){
-              //console.log("problem getting token back from server!", resp)
-              //  $location.url("/")
-            })
-
-
-
-
-
-    })
 
 
     .controller("MyPublicationsCtrl", function($scope, $location, $http, $auth){
